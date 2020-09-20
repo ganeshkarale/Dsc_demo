1e0e0472f1424729ec46b9dda18bcce6eb67996c
ganeshkarale <64553071+ganeshkarale@users.noreply.github.com>
2020-09-20 18:17:27 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class add
+{
+public static void main(String args[])
+{
+    Scanner in=new Scanner(System.in);
+    int a,b;
+    a=in.nextInt();
+    b=in.nextInt();
+    int c=a+b;
+    System.out.println("Addition is "+c);
+}
+}
